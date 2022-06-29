2755ad3bee5a0e52fbcebb56e38c5ce081d34968
VladiG <vladigrosjean@gmail.com>
2022-06-29 19:03:17 +0200

    some css

--- script.js
+++ script.js
@@ -20,35 +20,76 @@ window.onload = function(){
               console.log(element.title)
               console.log(element.prod_year)
               console.log(element.id)
+
               results.innerHTML +=    
               `<div class="result">
                   <div class="well text-center">
                     <h3>${element.title}</h3>
                     <p>${element.summary}</p>
                     <p>Production: ${element.prod_year}</p>
-                    <a onclick="getMovieId('${element.id}')" class="btn btn-primary" >Movie Details</a>
+                    <button onclick="getMovieId('${element.id}')" class="btn btn-primary" > + MORE</button>
                   </div>
                </div>`
                       });
+                 
                     })
-
+                
   
                   }
 
-    function getMovieId(id){
-      sessionStorage.setItem('movie_id', id);
+  function getMovieId(id){
+      sessionStorage.setItem('movie_id', element.id);
       window.location = 'movie.html';
       return false;
     }
-  
-        //box.innerHTML = (txt.slice(txt.length - 42));
+ 
+    /**Pagination */
+
+  const obj = [{N1, N2, N3, N4, N5}]
+    var current_page = 1;
+    var obj_per_page = 3;
+  function totNumPages(){
+      return Math.ceil(obj.length / obj_per_page);
+                  }         
+  function prevPage(){
+      if (current_page > 1) {
+          current_page--;
+          change(current_page);}
+      }
+  function nextPage(){
+      if (current_page < totNumPages()) {
+          current_page++;
+          change(current_page);}
+      }
+  function change(page){
+      var btn_next = document.getElementById("btn_next");
+      var btn_prev = document.getElementById("btn_prev");
+      var listing_table = document.getElementById("TableList");
+      var page_span = document.getElementById("page");
+          if (page < 1) page = 1;
+          if (page > totNumPages()) page = totNumPages();
+            listing_table.innerHTML = "";
+              for (var i = (page-1) * obj_per_page; i < (page * obj_per_page); i++) {
+              listing_table.innerHTML += obj[i].number + "<br>";
+                      }
+            page_span.innerHTML = page;
+              if (page == 1) {
+              btn_prev.style.visibility = "hidden";
+                } else {
+              btn_prev.style.visibility = "visible";
+                      }
+              if (page == totNumPages()) {
+              btn_next.style.visibility = "hidden";
+                } else {
+              btn_next.style.visibility = "visible";
+                      }
+                  }
 
             //let valueR = searchText.value;
 
        
                     
 /*
-
        fetch('http://localhost:3000/api/movies')
                 .then(response => { 
                     console.log(response);  
